3341a2db4b70e1a0822362244432ac201dbc68bc
PTrodri72 <rodrigofilipe631@gmail.com>
2026-04-24 22:24:11 +0100

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'Arial' }}>
-      <h1>Peralta License System</h1>
-      <p>API online ✔</p>
-    </div>
-  );
-}
